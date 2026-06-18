6d0292bdb9c8de08f3b31457b154644dc8dfd2f9
Khadija Absi <1220458@student.birzeit.edu>
2026-06-18 18:57:15 +0300

    update

--- app/src/main/java/com/example/a1220458_1220014_courseproject/activities/MainActivity.java
+++ app/src/main/java/com/example/a1220458_1220014_courseproject/activities/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.a1220458_1220014_courseproject.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,46 +10,22 @@ import com.example.a1220458_1220014_courseproject.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imgLogo;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imgLogo = findViewById(R.id.imgLogo);
-
-        Animation anim =
-                AnimationUtils.loadAnimation(this, R.anim.logo_animation);
-
         new Handler().postDelayed(() -> {
 
-            imgLogo.startAnimation(anim);
+            Intent intent = new Intent(
+                    MainActivity.this,
+                    IntroductionActivity.class
+            );
 
-            anim.setAnimationListener(new Animation.AnimationListener() {
-
-                @Override
-                public void onAnimationStart(Animation animation) {
-
-                }
-
-                @Override
-                public void onAnimationEnd(Animation animation) {
-
-                    Intent intent =
-                            new Intent(MainActivity.this,
-                                    IntroductionActivity.class);
-
-                    startActivity(intent);
-                    finish();
-                }
-
-                @Override
-                public void onAnimationRepeat(Animation animation) {
-
-                }
-            });
+            startActivity(intent);
+            finish();
 
         }, 3000);
+
     }
 }
