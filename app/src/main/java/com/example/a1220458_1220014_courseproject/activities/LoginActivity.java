c5ba9cf45d59c38c2c77302050073a5db11b4239
Khadija Absi <1220458@student.birzeit.edu>
2026-06-21 20:29:19 +0300

    email update save

--- app/src/main/java/com/example/a1220458_1220014_courseproject/activities/LoginActivity.java
+++ app/src/main/java/com/example/a1220458_1220014_courseproject/activities/LoginActivity.java
@@ -127,25 +127,16 @@ public class LoginActivity extends AppCompatActivity {
                 );
 
 
+                editor.putString("current_user_email", email);
+
                 if (cbRememberMe.isChecked()) {
 
-                    editor.putString(
-                            "email",
-                            email
-                    );
-
-                    editor.putBoolean(
-                            "remember",
-                            true
-                    );
+                    editor.putString("email", email);
+                    editor.putBoolean("remember", true);
 
                 } else {
 
-                    editor.remove("email");
-                    editor.putBoolean(
-                            "remember",
-                            false
-                    );
+                    editor.putBoolean("remember", false);
 
                 }
 
